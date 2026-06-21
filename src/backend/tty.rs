3431748252a43603b025f83dd0b3d79452977b38
Zhai Can <bczhc0@126.com>
2026-06-21 08:27:08 +0800

    bpc fixup

--- src/backend/tty.rs
+++ src/backend/tty.rs
@@ -2290,6 +2290,7 @@ impl Tty {
                 vrr_supported: false,
                 vrr_enabled: false,
                 logical,
+                max_bpc: None,
             };
 
             ipc_outputs.insert(*output_id, ipc_output);
